a185d6ad166a25e994ae04372d5974e5fe505c13
wlh <wlhmyit@126.com>
2014-08-09 19:08:21 +0800

    add monthPanel

--- calendar.js
+++ calendar.js
@@ -6,55 +6,53 @@ var Calendar = function(div){
 	//this.div = document.getElementById(div) || {width:800, height:600};
 };
 
-Calendar.prototype.show = function(date){
-	var date = date || new Date(),
-	    year = date.getFullYear(),
-	    month = date.getMonth(),
-	    day = date.getDate(),
-	    week = date.getDay(),//星期几
-
-	    //width = this.div.width || 800,
-	    //height = this.div.height || 600;
-	    //cell = {width: width/7, height: height/5},
-	    currentDays = new Date(year, month + 1, 0).getDate(), //本月天数
-	    preDays = new Date(year, month, 0).getDate(); //前月的天数
+Calendar.prototype.showUI = function(date){
+	//width = this.div.width || 800,
+	//height = this.div.height || 600;
+	//cell = {width: width/7, height: height/5},
 	//创建单元格
 	//var cellDOM = document.createElement('div');
 	//cellDOM.style.width = cell.width + 'px';
 	//cellDOM.style.height = cell.height + 'px';
 	//cellDOM.style.display = 'inline-block';
 	//cellDOM.style.float = 'left';
-	console.log(date);
-	var firstDay = new Date(year, month, 1),
-	    firstCell = firstDay.getDay() === 0 ? 6 : firstDay.getDay() - 1;//计算本月1号出现的地方
-	    bottomCell =  35 - currentDays - firstCell; //剩余的下个月显示 
+};
 
-	//两个约束：星期一在第一列，一个月在一个面上
-	//显示前一个月
+Calendar.prototype.monthPanel = function(date){
+	//如果传递了Date对象，则按Date对象进行计算月份面板
+	//否则，按照当前月份计算面板
+	var date = date || new Date(),
+	    year = date.getFullYear(),
+	    month = date.getMonth(),
+	    day = date.getDate(),
+	    week = date.getDay(),
+	    currentDays = new Date(year, month + 1, 0).getDate(),
+	    preDays = new Date(year, month, 0).getDate(),
+	    firstDay = new Date(year, month, 1),
+	    firstCell = firstDay.getDay() === 0 ? 6 : firstDay.getDay() - 1,
+	    bottomCell =  35 - currentDays - firstCell;
+	//前一个月该显示多少天
 	var preMonth = [];
-	for(var i = firstCell; i > 0; i--){
-		preMonth.push(new Date(year, month - 1, preDays - i + 1));
+	for(var p = firstCell; p > 0; p--){
+		preMonth.push(new Date(year, month - 1, preDays - p + 1));
 	}
-	//显示本月
+	//本月
 	var currentMonth = [];
 	for(var c = 0; c < currentDays; c++){
 		currentMonth.push(new Date(year, month, c + 1));
 	}
-	//显示后一个月
+	//下一个月
     var nextMonth = [];
     for(var n = 0; n < bottomCell; n++){
     	nextMonth.push(new Date(year, month + 1, n + 1));
     }
 
-    var showArr = [];
-    showArr = showArr.concat(preMonth, currentMonth, nextMonth);
-
-    console.log(showArr);
-
+    preMonth = preMonth.concat(currentMonth, nextMonth);
+    return preMonth;
 };
 
 var c = new Calendar('div');
-c.show(new Date(2014, 6, 1));
+console.log(c.monthPanel(new Date(2014, 6, 12)));
 
 
 
